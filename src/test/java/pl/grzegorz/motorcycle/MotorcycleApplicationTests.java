ee27009fe59f18d5fb338d4244158e9c24824a09
grzesiek1362 <leslie.oladele@gmail.com>
2022-01-15 23:42:23 +0100

    creating motorcycle app project and adding dependencies

---
+++ src/test/java/pl/grzegorz/motorcycle/MotorcycleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.grzegorz.motorcycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotorcycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
